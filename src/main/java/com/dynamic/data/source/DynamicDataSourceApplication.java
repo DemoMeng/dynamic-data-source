fa29430f040f8a1151bd23df3ee675c308431f98
蒙启章 <mengqizhang@huluwa.cc>
2020-09-08 16:40:36 +0800

    init by mqz

---
+++ src/main/java/com/dynamic/data/source/DynamicDataSourceApplication.java
@@ -0,0 +1,16 @@
+package com.dynamic.data.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mqz
+ */
+@SpringBootApplication
+public class DynamicDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDataSourceApplication.class, args);
+    }
+
+}
